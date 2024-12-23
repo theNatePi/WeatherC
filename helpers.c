e870e3e4d6ceabdca879606d20b6b9da0c0bdd3a
Nathan Pietrantonio <pietrann@uci.edu>
2024-12-23 13:13:30 -0800

    Setup GPIO handlers
    Create helper function to handle GPIO

--- helpers.c
+++ helpers.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <lgpio.h>
+
+// API Helpers
 
 double getPrecipitation(char *response) {
   // Get the heders
@@ -22,4 +25,39 @@ double getPrecipitation(char *response) {
   }
   result = strtok(NULL, ",");
   return atof(result);
+}
+
+// GPIO Helpers
+
+int updateLeds(double current[6], double history[6]) {
+  int exitVal = 0;
+
+  int handle = lgGpiochipOpen(0);
+  if (handle < 0) {
+    perror("Failed to open gpiochip");
+    return -1;
+  }
+
+  for (int i = 0; i < 6; i++) {
+    if (lgGpioClaimOutput(handle, 0, LEDS[i], 0) < 0) {
+      perror("Failed to claim output");
+      exitVal = -1;
+      goto cleanup;
+    }
+  }
+
+  for (int i = 0; i < 6; i++) {
+    if (current[i] > 0) {
+      lgGpioWrite(handle, LEDS[j], 1);
+    } else {
+      lgGpioWrite(handle, LEDS[j], 0);
+    }
+  }
+
+  cleanup:
+  for (i = 0; i < 6; i++) {
+    lgGpioFree(handle, leds[i]);
+  }
+  lgGpiochipClose(handle);
+  return exitVal;
 }
--- include/helpers.h
+++ include/helpers.h
@@ -1,6 +1,8 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
+int LEDS[6] = {15, 14, 17, 4, 3, 2};
+
 double getPrecipitation(char *response);
 
 #endif //HELPERS_H
