84cb9f37c7810232a9bed174fa33a501b6c9e5b6
Nathan Pietrantonio <pietrann@uci.edu>
2024-12-23 10:03:41 -0800

    feat: extract precip from json

---
+++ helpers.c
@@ -0,0 +1,26 @@
+#include "include/helpers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+double getPrecipitation(char *response) {
+  // Get the heders
+  strtok(response, "\n\n");
+  // Skip all headers
+  for (int i = 0; i < 22; i++) {
+    strtok(NULL, "\n\n");
+  }
+
+  // Get to the JSON body
+  response = strtok(NULL, "\n\n");
+
+  // Skip through all values to get the precipitation
+  // Measured in mm
+  char *result = strtok(response, ":");
+  for (int i = 0; i < 27; i++) {
+    result = strtok(NULL, ":");
+  }
+  result = strtok(NULL, ",");
+  printf("%f\n", atof(result));
+  return atof(result);
+}
---
+++ include/helpers.h
@@ -0,0 +1,6 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+double getPrecipitation(char *response);
+
+#endif //HELPERS_H
