e5ba85057486e6b319c550c95f96b029b19cbe7f
Nathan Pietrantonio <pietrann@uci.edu>
2024-12-23 13:13:16 -0800

    Setup threads
    Create single thread to handle updating LED status every 20 min

--- include/main.h
+++ include/main.h
@@ -1,4 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define ENV_PATH ".env"
+#define API_URL "http://api.weatherapi.com"
+
+double HISTORY[6];
+double CURRENT[6];
+
+char *CITIES[6] = {
+    "Tokyo",
+    "San Francisco",
+    "Seattle",
+    "New York",
+    "Vancouver",
+    "London"
+};
+
+void handle_sigint(int _);
+void *updateThread(void* _);
+int main(void);
+
 #endif //MAIN_H
--- main.c
+++ main.c
@@ -1,12 +1,111 @@
 #include "include/main.h"
 #include "include/env_loader.h"
+#include "include/requests.h"
+#include "include/helpers.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include <signal.h>
+
+sig_atomic_t sigint = 0;
+pthread_mutex_t CURRENT_LOCK;
+pthread_mutex_t HISTORY_LOCK;
+
+void handle_sigint(int _) {
+    sigint = 1;
+}
+
+void *updateThread(void* _) {
+    char api_key[50];
+    char *pKey = api_key;
+    if (loadEnv("WEATHER_KEY", &pKey, ENV_PATH) < 0) {
+        perror("Failed to load environment variable WEATHER_KEY");
+        return NULL;
+    }
+
+    while (1) {
+        for (int i = 0; i < 6; i++) {
+            char buff[200];
+            char city[200];
+            strcpy(city, CITIES[i]);
+            for (int j = 0; j < (int) strlen(city); j++) {
+                if (city[j] == ' ') {
+                    city[j] = '+';
+                }
+            }
+            snprintf(buff, 200, "v1/current.json?key=%s&q=%s&aqi=no", pKey, city);
+
+            char *api_recv = malloc(10000);
+            if (get(API_URL, buff, &api_recv) < 0) {
+                perror("Failed to get API URL");
+                free(api_recv);
+                return NULL;
+            }
+
+            if (api_recv == NULL) {
+                perror("API response invalid");
+                free(api_recv);
+                return NULL;
+            }
+
+            // TODO: Get the response code
+
+            double prec = getPrecipitation(api_recv);
+            free(api_recv);
+
+            pthread_mutex_lock(&CURRENT_LOCK);
+            pthread_mutex_lock(&HISTORY_LOCK);
+            if (HISTORY[i] > 0) {
+                if (HISTORY[i] < 0.01) {
+                    HISTORY[i] = 0;
+                } else {
+                    HISTORY[i] -= 0.01;
+                }
+            }
+            CURRENT[i] = prec;
+            HISTORY[i] += prec;
+            pthread_mutex_unlock(&CURRENT_LOCK);
+            pthread_mutex_unlock(&HISTORY_LOCK);
+        }
+
+        for (int i = 0; i < 60; i++) {
+            sleep(20);
+            if (sigint) {
+                return NULL;
+            }
+        }
+    }
+}
 
 int main(void) {
-  char *key;
-  int result = loadEnv("WEATHER_KEY", &key, ".env");
-  printf("%s\n", key);
-  free(key);
-  return 0;
+    signal(SIGINT, handle_sigint);
+
+    if (pthread_mutex_init(&CURRENT_LOCK, NULL) != 0) {
+        perror("Failed to initialize mutex");
+        return 1;
+    }
+    if (pthread_mutex_init(&HISTORY_LOCK, NULL) != 0) {
+        perror("Failed to initialize mutex");
+        return 1;
+    }
+
+    pthread_t threadId;
+    pthread_create(&threadId, NULL, updateThread, NULL);
+
+    while (1) {
+        if (sigint) {
+            printf("Shutting down...\nThis may take up to 20 seconds.");
+            fflush(stdout);
+            pthread_join(threadId, NULL);
+            pthread_mutex_destroy(&CURRENT_LOCK);
+            pthread_mutex_destroy(&HISTORY_LOCK);
+            return 0;
+        }
+
+        // update LEDs
+        sleep(1);
+
+    }
 }
