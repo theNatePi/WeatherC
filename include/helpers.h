53b261a5e15ced08e024e7e712aaf6d07d817b4e
Nathan Pietrantonio <pietrann@uci.edu>
2024-12-25 23:16:42 -0800

    Add values for rain level

--- include/helpers.h
+++ include/helpers.h
@@ -1,7 +1,11 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
+#define LOW_RAIN 20
+#define MED_RAIN 50
+#define HIGH_RAIN 100
+
 double getPrecipitation(char *response);
 int updateLeds(double current[6], double history[6], int handle, int leds[6]);
 
-#endif //HELPERS_H
+#endif // HELPERS_H
